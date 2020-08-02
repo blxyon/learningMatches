ed8433d2fbe185e216b1a732e8ae18430eb1bc79
Valentin Ionut Burlacu <blxyon@yahoo.com>
2020-08-02 01:10:12 +0100

    v1.4 functionality regarding repetition of reminders. If come from another version use alter to add a new column to your db. The rest shall run fine

--- Reminder.py
+++ Reminder.py
@@ -18,17 +18,20 @@ class ReminderFr(tk.Frame):
         self.removeButtons=[]
         self.removedRows=[]
         self.sessionAddedRem=[]
+
         
         curYear=getCurrentTimeToList()[2]
+        repDaysList=["Repetition time"]+list(range(1,101))
         monthOptions = ["Month","1","2","3","4","5","6","7","8","9","10","11","12"]
         yearOptions =[curYear,str(int(curYear)+1),str(int(curYear)+2),str(int(curYear)+3),str(int(curYear)+4)]
         dayOptions=["Select Month"]
 
-        self.addRemainderButton=tk.Button(self,text="Add remainder",command=lambda:self.add_remainder(butType="gen"))
+        self.addRemainderButton=tk.Button(self,text="Add reminder",command=lambda:self.add_reminder("gen"))
         self.addRemainderButton.grid(row=2,column=0)
 
-        self.addRem1wEEK=tk.Button(self,text="Add remainder for a week from now",command=lambda:self.add_remainder(butType="1week"))
+        self.addRem1wEEK=tk.Button(self,text="Add reminder for a week from now",command=lambda:self.add_reminder("1week"))
         self.addRem1wEEK.grid(row=3,column=0)
+
         
         self.remainderVar = tk.StringVar()
         self.addTextBox=tk.Entry(self,textvariable=self.remainderVar)
@@ -51,44 +54,104 @@ class ReminderFr(tk.Frame):
         self.dayOption=tk.OptionMenu(self,self.remainderDay, *dayOptions)
         self.dayOption.grid(row=0,column=2,sticky="w")
 
-        self.initialize_remainders()
-    def add_remainder(self,butType):
-        month=self.remainderMonth.get()
-        
-        now = datetime.now()
-        dt_string2 = now.strftime("%d/%m/%Y %H:%M:%S")
-        #
-        
-        reminderStr=self.remainderVar.get()
-        if(reminderStr==""):
-            reminderStr="_not_specified"
+        self.repDays=tk.StringVar()
+        self.repDays.set(repDaysList[0])
+        self.repDaysOption=tk.OptionMenu(self,self.repDays,*repDaysList)
+        self.repDaysOption.grid(row=2,column=1)
 
-        if(month!="Month" and butType=="gen"):
-            year=self.remainderYear.get()
-            day=self.remainderDay.get()
+        self.initialize_remainders()
+    def add_reminder(self,*args):
+        #inspired by : https://stackoverflow.com/questions/5079609/methods-with-the-same-name-in-one-class-in-python
+        if len(args)==1 and isinstance(args[0],str):
+            butType=args[0]
+            month=self.remainderMonth.get()
             
-            dt_string = ("%s/%s/%s"%(day,month,year,))#string format
+            now = datetime.now()
+            nowDateStrHour = now.strftime("%d/%m/%Y %H:%M:%S")
             
-            self.add_reminder(reminderStr,dt_string,dt_string2)
-        elif(butType=="1week"):
-            newDate=now+timedelta(days=7)
-            dt_string=newDate.strftime("%d/%m/%Y")
-            self.add_reminder(reminderStr,dt_string,dt_string2)
-            
-    def add_reminder(self,reminderStr,dt_string,dt_string2):
-        now = datetime.now()
-        todaysDateString=("%s/%s/%s"%(getCurrentTimeToList()[0],getCurrentTimeToList()[1],getCurrentTimeToList()[2],))
-        appendDB=self.parent.db.add_reminder_to_table(reminderStr,dt_string,todaysDateString)
-        todaysDate=now.date()
-        #we must update the gui.. if necessary
-        dateRemainder = datetime.strptime(dt_string,"%d/%m/%Y").date()
-        if(todaysDate<=dateRemainder and appendDB==True):
+            reminderStr=self.remainderVar.get()
+            if(reminderStr==""):
+                reminderStr="_not_specified"
                 
-            line1="    * "+reminderStr+", added for the time:"+dt_string+", at:"+dt_string2+"\n"
-            self.parent.txt.writeAndDisplay(line1)
-            self.sessionAddedRem.append((reminderStr,dt_string))
-            self.removeAllRem()
-            self.initialize_remainders()   
+            repetition=self.repDays.get()
+                
+            if(repetition=="Repetition time"):
+                repetition="None"
+                
+            if(month!="Month" and butType=="gen"):
+                year=self.remainderYear.get()
+                day=self.remainderDay.get()
+                
+                
+                dateReminder = ("%s/%s/%s"%(day,month,year,))#string format
+                
+                self.add_reminder(reminderStr,dateReminder,nowDateStrHour,repetition)
+            elif(butType=="1week"):
+                newDate=now+timedelta(days=7)
+                newDateStr=newDate.strftime("%d/%m/%Y")
+                self.add_reminder(reminderStr,newDateStr,nowDateStrHour,repetition)
+        elif len(args)==4:
+            reminderStr=args[0]
+            dateReminderStr=args[1]
+            nowDateStrHour=args[2]
+            repetition=args[3]
+            
+            now = datetime.now()
+            todaysDateString=now.strftime("%d/%m/%Y")#("%s/%s/%s"%(getCurrentTimeToList()[0],getCurrentTimeToList()[1],getCurrentTimeToList()[2],))
+            appendDB=self.parent.db.add_reminder_to_table(reminderStr,dateReminderStr,todaysDateString,repetition)
+            todaysDate=now.date()
+            #we must update the gui.. if necessary
+            dateRemainder = datetime.strptime(dateReminderStr,"%d/%m/%Y").date()
+            dateRemainder = self.calculateCurrentRep(repetition,dateRemainder,todaysDate)
+            if(todaysDate<=dateRemainder and appendDB==True):
+                line1="    * "+reminderStr+", added for the time:"+dateReminderStr+", at:"+nowDateStrHour+", repeats every: "+repetition+" days"+"\n"
+                self.parent.txt.writeAndDisplay(line1)
+                self.sessionAddedRem.append((reminderStr,dateReminderStr,todaysDateString,repetition))
+                self.removeAllRem()
+                self.initialize_remainders()
+            
+    
+    def checkColourToUse(self,todaysDate,dateRemainder):
+        colour="white"
+        if todaysDate==dateRemainder:
+            todayStr="Today!- "
+            colour="red"
+        else:
+            if todaysDate==(dateRemainder-timedelta(days=1)):
+                colour="yellow"
+            todayStr=""
+        return (colour,todayStr)
+    def formatLabel(self,todayStr,label,dateReminder,colour):
+        if str(type(label[2]))=="<class 'NoneType'>":
+            rep="_None"
+        else:
+            rep=label[2]
+        
+        if label in self.sessionAddedRem:
+            l=tk.Label(self,text=todayStr+"*Reminder: "+label[0]+", on the date: "+str(dateReminder)+", repeats every: "+rep+" days",bg=colour)
+        else:
+            l=tk.Label(self,text=todayStr+"Reminder: "+label[0]+", on the date: "+str(dateReminder)+", repeats every: "+rep+" days",bg=colour)
+        return l
+    def calculateCurrentRep(self,rep,startDate,todaysDate):
+        #print(str(type(rep)))
+        if(rep!="None" and str(type(rep))!="<class 'NoneType'>"):
+            delta=todaysDate-startDate# if this is negative that means we shall return the start date because we do not need to repeat it and is good to be displayed
+            rep=int(rep)
+            if delta.days<0:
+                return startDate
+            else:
+                daysBetween=delta.days#include the start date too
+                if daysBetween==0:
+                    return startDate#they are the same so we shall return the reminder to be for today as well
+                print("daysbetween:"+str(daysBetween))
+                print(str(startDate)+", "+str(todaysDate))
+                noDaysLeft=daysBetween%rep
+                
+                print("noDaysLeft:"+str(noDaysLeft))
+                newDate=todaysDate+timedelta(days=-noDaysLeft+rep)
+                return newDate
+        else:
+            return startDate
     def initialize_remainders(self):
         self.lastRemainderOnRow=0
         self.rowLabels=[]
@@ -98,33 +161,25 @@ class ReminderFr(tk.Frame):
             todaysTime=datetime.now()
             todaysDate=todaysTime.date()
             for label in self.parent.db.rem:
-                dateRemainder = datetime.strptime(label[1],"%d/%m/%Y").date()         
+                #print(label)
+                dateRemainder = datetime.strptime(label[1],"%d/%m/%Y").date()
+                dateRemainder = self.calculateCurrentRep(label[2],dateRemainder,todaysDate)
                 if(todaysDate<=dateRemainder):
-                    colour="white"
-                    if todaysDate==dateRemainder:
-                        todayStr="Today!- "
-                        colour="red"
-                    else:
-                        if todaysDate==(dateRemainder-timedelta(days=1)):
-                            colour="yellow"
-                        todayStr=""
-                    if label in self.sessionAddedRem:
-                        l=tk.Label(self,text=todayStr+"*Reminder: "+label[0]+", on the date: "+label[1],bg=colour)
-                    else:
-                        l=tk.Label(self,text=todayStr+"Reminder: "+label[0]+", on the date: "+label[1],bg=colour)
+                    colour, todayStr=self.checkColourToUse(todaysDate,dateRemainder)
+                    l=self.formatLabel(todayStr,label,dateRemainder,colour)
                     l.grid(row=self.lastRemainderOnRow,column=3)
                     self.rowLabels.append(l)
                     
-                    b=tk.Button(self,text="Remove",command=lambda index=self.lastRemainderOnRow,remStr=label[0], dd=label[1]:self.deleteReminder(index,remStr,dd))
+                    b=tk.Button(self,text="Remove",command=lambda index=self.lastRemainderOnRow,remStr=label[0], dd=label[1], rep=label[2]:self.deleteReminder(index,remStr,dd,rep))
                     self.removeButtons.append(b)
                     self.removeButtons[len(self.removeButtons)-1].grid(row=self.lastRemainderOnRow,column=4)
                 
                     self.lastRemainderOnRow=self.lastRemainderOnRow+1
-    def deleteReminder(self,index,desc,due_date):
-        self.parent.db.remove_reminder(desc,due_date)
+    def deleteReminder(self,index,desc,due_date,repetition):
+        self.parent.db.remove_reminder(desc,due_date,repetition)
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-        line1="    x "+desc+", added for the time:"+due_date+", removed at:"+dt_string+"\n"
+        line1="    x "+desc+", added for the time:"+due_date+", with repetition:"+repetition+", was removed at:"+dt_string+"\n"
         self.parent.txt.writeAndDisplay(line1)
         
         self.removeAllRem()
--- dbHandler.py
+++ dbHandler.py
@@ -14,33 +14,40 @@ class DatabaseApp:
         (ID_reminder integer PRIMARY KEY, 
         description text, 
         due_date text, 
-        creation_date text) 
+        creation_date text,
+        days_repetition text) 
         ''')
 
     def create_tables(self):
         self.create_reminder_table()
-    def add_reminder_to_table(self,desc,due_date,creation_date):
-        if self.getSReminder(desc,due_date)==[]:
-            item=(desc,due_date,creation_date)
+    def add_reminder_to_table(self,desc,due_date,creation_date,repetition):
+        if self.getSReminder(desc,due_date,repetition)==[]:
+            item=(desc,due_date,creation_date,repetition)
             self.cursorSystemDatabase.execute('''INSERT INTO
                                             Reminder(description,
                                             due_date,
-                                            creation_date) values (?,?,?)''',item)
+                                            creation_date,
+                                            days_repetition) values (?,?,?,?)''',item)
             self.commit()
             self.select_all_reminders()#updating the reminders list
             return True#as in we did append to the db
         return False
-    def remove_reminder(self,desc,due_date):
-        self.cursorSystemDatabase.execute('''DELETE FROM
-                                        Reminder WHERE description=? AND due_date=?''',(desc,due_date,))
+    def remove_reminder(self,desc,due_date,repetition):
+        print(self.getSReminder(desc,due_date,repetition))
+        if repetition!=None:#this solves the earlier app versions problems where they would have an empty cell in the days_repetition column
+            self.cursorSystemDatabase.execute('''DELETE FROM
+                                        Reminder WHERE description=? AND due_date=? AND days_repetition=?''',(desc,due_date,repetition))
+        else:
+            self.cursorSystemDatabase.execute('''DELETE FROM
+                                        Reminder WHERE description=? AND due_date=? AND days_repetition is NULL''',(desc,due_date))
         self.commit()
         self.select_all_reminders()
     def select_all_reminders(self):
-        self.cursorSystemDatabase.execute('''SELECT description,due_date from Reminder ORDER BY ID_reminder DESC''')#order desc to ensure we show the most recent added ones
+        self.cursorSystemDatabase.execute('''SELECT description,due_date,days_repetition from Reminder ORDER BY ID_reminder DESC''')#order desc to ensure we show the most recent added ones
         self.rem=self.cursorSystemDatabase.fetchall()
-    def getSReminder(self,desc,due_date):
+    def getSReminder(self,desc,due_date,repetition):
         self.cursorSystemDatabase.execute('''SELECT description,due_date from Reminder WHERE
-                                                description=? AND due_date=?''',(desc,due_date,))
+                                                description=? AND due_date=? AND days_repetition=?''',(desc,due_date,repetition,))
         selRem=self.cursorSystemDatabase.fetchall()
         return selRem
     def closedb(self): 
